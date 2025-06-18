ec44759fec1be473b1633059ace0ccef45e32f25
Thiagoo-Franca <tfranca@ufba.br>
2025-06-17 22:40:21 -0300

    correcao da atividade 03, principais correcoes: agora sendo consumida uma API tanto adicionar pizzas ao carrinho quanto para remover. Alem disso, foi corrigido o bug do carrossel.

---
+++ atividade03/components/Reviews/Reviews.tsx
@@ -0,0 +1,13 @@
+import styles from './Reviews.module.css'
+
+export default function Reviews() {
+    return (
+        <section className={styles.Reviews}>
+            <main>
+                <img src="/assets/ReviewsImg/Format quote.png" alt="" />
+                <h1>Melhor margherita do mundo</h1>
+                <h2>Alguém importante</h2>
+            </main>
+        </section>
+    );
+}
